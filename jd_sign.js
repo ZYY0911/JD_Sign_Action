2bcad1a260fe31c68dc0ebdfab3d1a7c813335c8
ZYY0911 <605061405@qq.com>
2021-03-08 09:14:52 +0800

    修改时间

--- jd_sign.js
+++ jd_sign.js
@@ -26,133 +26,137 @@ const result_path = './result.txt'
 const error_path = './error.txt'
 
 Date.prototype.Format = function (fmt) {
-  var o = {
-    'M+': this.getMonth() + 1,
-    'd+': this.getDate(),
-    'H+': this.getHours(),
-    'm+': this.getMinutes(),
-    's+': this.getSeconds(),
-    'S+': this.getMilliseconds()
-  };
-  if (/(y+)/.test(fmt)) {
-    fmt = fmt.replace(RegExp.$1, (this.getFullYear() + '').substr(4 - RegExp.$1.length));
-  }
-  for (var k in o) {
-    if (new RegExp('(' + k + ')').test(fmt)) {
-      fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (('00' + o[k]).substr(String(o[k]).length)));
+    var o = {
+        'M+': this.getMonth() + 1,
+        'd+': this.getDate(),
+        'H+': this.getHours(),
+        'm+': this.getMinutes(),
+        's+': this.getSeconds(),
+        'S+': this.getMilliseconds()
+    };
+    if (/(y+)/.test(fmt)) {
+        fmt = fmt.replace(RegExp.$1, (this.getFullYear() + '').substr(4 - RegExp.$1.length));
     }
-  }
-  return fmt;
+    for (var k in o) {
+        if (new RegExp('(' + k + ')').test(fmt)) {
+            fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (('00' + o[k]).substr(String(o[k]).length)));
+        }
+    }
+    return fmt;
 };
 
 function setupCookie() {
-  var js_content = fs.readFileSync(js_path, 'utf8')
-  js_content = js_content.replace(/var Key = ''/, `var Key = '${cookie}'`)
-  fs.writeFileSync(js_path, js_content, 'utf8')
+    var js_content = fs.readFileSync(js_path, 'utf8')
+    js_content = js_content.replace(/var Key = ''/, `var Key = '${cookie}'`)
+    fs.writeFileSync(js_path, js_content, 'utf8')
 }
+
 let reMindMsg = {
-  // 消息标题
-  text: "❌ 蘑菇丁签到失败了，请检查 ❌",
-  // 消息主体
-  desp: "请检查账号密码或Token（如果存在）是否失效。其他问题请联系ZYY0911！",
+    // 消息标题
+    text: "❌ 蘑菇丁签到失败了，请检查 ❌",
+    // 消息主体
+    desp: "请检查账号密码或Token（如果存在）是否失效。其他问题请联系ZYY0911！",
 };
+
 async function sendNotificationIfNeed() {
 
-  if (!push_key) {
-    console.log('执行任务结束!');
-    return;
-  }
+    if (!push_key) {
+        console.log('执行任务结束!');
+        return;
+    }
 
-  if (!fs.existsSync(result_path)) {
-    console.log('没有执行结果，任务中断!');
-    return;
-  }
+    if (!fs.existsSync(result_path)) {
+        console.log('没有执行结果，任务中断!');
+        return;
+    }
 
-  let text = "京东签到_" + new Date().Format('yyyy.MM.dd');
-  let desp = fs.readFileSync(result_path, "utf8")
-  // 去除末尾的换行
-  let SCKEY = push_key.replace(/[\r\n]/g, "")
-  let TG_BOT_TOKEN = tgbottoken.replace(/[\r\n]/g, "")
-  let TG_USER_ID = tguserid.replace(/[\r\n]/g, "")
-  let config = {
-    // 用户手机号
-    tgtocken: TG_BOT_TOKEN,
-    // 用户密码
-    tgid: TG_USER_ID
-  };
-  // const options ={
-  //   uri:  `https://sc.ftqq.com/${SCKEY}.send`,
-  //   form: { text, desp },
-  //   json: true,
-  //   method: 'POST'
-  // }
-  // const options ={
-  //     uri:  `https://api.telegram.org/bot${TG_BOT_TOKEN}/sendMessage`,
-  //     data: `chat_id=${TG_USER_ID}&text=${text}\n\n${desp}&disable_web_page_preview=true`,
-  //     method: 'POST',
-  //     headers: {'Content-Type': 'application/x-www-form-urlencoded'}
-  //   }
-  //
-  //   rp.post(options).then(res=>{
-  //     const code = res['errno'];
-  //     if (code == 0) {
-  //       console.log("通知发送成功，任务结束！")
-  //     }
-  //     else {
-  //       console.log(res);
-  //       console.log("通知发送失败，任务中断！")
-  //       fs.writeFileSync(error_path, JSON.stringify(res), 'utf8')
-  //     }
-  //   }).catch((err)=>{
-  //     console.log("通知发送失败，任务中断！")
-  //     fs.writeFileSync(error_path, err, 'utf8')
-  //   })
-  let msg = await TGSend(axios, config, reMindMsg);
-  console.log(msg);
+    let text = "京东签到_" + new Date().Format('yyyy.MM.dd');
+    let desp = fs.readFileSync(result_path, "utf8")
+    // 去除末尾的换行
+    let SCKEY = push_key.replace(/[\r\n]/g, "")
+    let TG_BOT_TOKEN = tgbottoken.replace(/[\r\n]/g, "")
+    let TG_USER_ID = tguserid.replace(/[\r\n]/g, "")
+    let config = {
+        // 用户手机号
+        tgtocken: TG_BOT_TOKEN,
+        // 用户密码
+        tgid: TG_USER_ID
+    };
+    // const options ={
+    //   uri:  `https://sc.ftqq.com/${SCKEY}.send`,
+    //   form: { text, desp },
+    //   json: true,
+    //   method: 'POST'
+    // }
+    // const options ={
+    //     uri:  `https://api.telegram.org/bot${TG_BOT_TOKEN}/sendMessage`,
+    //     data: `chat_id=${TG_USER_ID}&text=${text}\n\n${desp}&disable_web_page_preview=true`,
+    //     method: 'POST',
+    //     headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+    //   }
+    //
+    //   rp.post(options).then(res=>{
+    //     const code = res['errno'];
+    //     if (code == 0) {
+    //       console.log("通知发送成功，任务结束！")
+    //     }
+    //     else {
+    //       console.log(res);
+    //       console.log("通知发送失败，任务中断！")
+    //       fs.writeFileSync(error_path, JSON.stringify(res), 'utf8')
+    //     }
+    //   }).catch((err)=>{
+    //     console.log("通知发送失败，任务中断！")
+    //     fs.writeFileSync(error_path, err, 'utf8')
+    //   })
+    let msg = await TGSend(axios, config, reMindMsg);
+    console.log(msg);
 }
 
 function main() {
 
-  if (!cookie) {
-    console.log('请配置京东cookie!'); return;
-  }
-
-  // 1、下载脚本
-  download(js_url, './').then(res=>{
-    // 2、替换cookie
-    setupCookie()
-    // 3、执行脚本
-    exec(`node '${js_path}' >> '${result_path}'`);
-    // 4、发送推送
-    let text = "京东签到_" + new Date().Format('yyyy.MM.dd');
-    let desp = fs.readFileSync(result_path, "utf8")
-    let TG_BOT_TOKEN = tgbottoken.replace(/[\r\n]/g, "")
-    let TG_USER_ID = tguserid.replace(/[\r\n]/g, "")
-    const options ={
-      uri:  `https://api.telegram.org/bot${TG_BOT_TOKEN}/sendMessage`,
-      data: `chat_id=${TG_USER_ID}&text=${text}\n\n${desp}&disable_web_page_preview=true`,
-      method: 'POST',
-      headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+    if (!cookie) {
+        console.log('请配置京东cookie!');
+        return;
     }
 
-    rp.post(options).then(res=>{
-      if (res.ok) {
-        console.log("通知发送成功，任务结束！")
-      }
-      else {
-        console.log(res);
-        console.log("通知发送失败，任务中断！")
-        fs.writeFileSync(error_path, JSON.stringify(res), 'utf8')
-      }
-    }).catch((err)=>{
-      console.log(err)
-      console.log("通知发送失败，任务中断！")
-      fs.writeFileSync(error_path, err, 'utf8')
+    // 1、下载脚本
+    download(js_url, './').then(res => {
+        // 2、替换cookie
+        setupCookie()
+        // 3、执行脚本
+        exec(`node '${js_path}' >> '${result_path}'`);
+        // 4、发送推送
+        let text = "京东签到_" + new Date().Format('yyyy.MM.dd');
+        let desp = fs.readFileSync(result_path, "utf8");
+        let TG_BOT_TOKEN = tgbottoken.replace(/[\r\n]/g, "");
+        let TG_USER_ID = tguserid.replace(/[\r\n]/g, "");
+        let desp_2 = desp.replace("+", "").replace("\n", "").replace("'","")
+
+        const options = {
+            uri: `https://api.telegram.org/bot${TG_BOT_TOKEN}/sendMessage`,
+            data: `chat_id=${TG_USER_ID}&text=${text}${desp_2}&disable_web_page_preview=true`,
+            method: 'POST',
+            headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+        }
+
+        rp.post(options).then(res => {
+            if (res.ok) {
+                console.log("通知发送成功，任务结束！")
+            } else {
+                console.log(res);
+                console.log("通知发送失败，任务中断！")
+                fs.writeFileSync(error_path, JSON.stringify(res), 'utf8')
+            }
+        }).catch((err) => {
+            console.log(err)
+            console.log("通知发送失败，任务中断！")
+            fs.writeFileSync(error_path, err, 'utf8')
+        })
+    }).catch((err) => {
+        console.log('脚本文件下载失败，任务中断！');
+        fs.writeFileSync(error_path, err, 'utf8')
     })
-  }).catch((err)=>{
-    console.log('脚本文件下载失败，任务中断！');
-    fs.writeFileSync(error_path, err, 'utf8')
-  })
 
 }
 
